8e94272e7d04df2123ee67d79ea3abd70f54023d
Jose M. Tobar <ilovemysillybanana@gmail.com>
2017-11-17 17:55:20 -0600

    Finished Windows JSON.

--- src/main/kotlin/com.platano.quienes.platform.windows.kt
+++ src/main/kotlin/com.platano.quienes.platform.windows.kt
@@ -13,7 +13,7 @@ class windows {
     private val regcompl = Regex("The command compl.*")
 
     fun console(cmd : String): BufferedReader {
-        var output = BufferedReader(
+        val output = BufferedReader(
                 InputStreamReader(
                         Runtime.getRuntime().exec(cmd)
                                 .inputStream))
@@ -22,7 +22,7 @@ class windows {
     }
 
     fun userData(output: BufferedReader): MutableList<String> {
-        var users = mutableListOf<String>()
+        val users = mutableListOf<String>()
         output.lines().forEach line@ {
             when {
                 it.isBlank()         -> return@line
@@ -32,7 +32,7 @@ class windows {
                 regdashs.matches(it) -> return@line
                 else -> {
                     it.split("            ").forEach {
-                        var name = it.replace("\\s".toRegex(), "")
+                        val name = it.replace("\\s".toRegex(), "")
                         if ( it.isNotBlank()){
                             //println("\"$name\"")
                             users.add(name)
@@ -48,17 +48,17 @@ class windows {
     //this function takes in a list of users, and returns its corresponding groups
     fun groupData(users: MutableList<String>): MutableMap<String, MutableList<String>> {
         //only lines we care about are username and local and global groups
-        val regexusr = Regex("User name.*")
+        //val regexusr = Regex("User name.*")
         val rexgroup = Regex(".*Group.*")
-        var groups = mutableMapOf<String, MutableList<String>>()
+        val groups = mutableMapOf<String, MutableList<String>>()
 
         users.forEach {
             println("This is the user: $it")
 
             //run command to get output
-            var user_group_data = console("net user $it")
-            var lsGroupData = mutableListOf<String>()
-            var name = it
+            val user_group_data = console("net user $it")
+            val lsGroupData = mutableListOf<String>()
+            val name = it
 
 
             user_group_data.lines().forEach line@ {
@@ -70,9 +70,10 @@ class windows {
                         // NOTE, each user group starts with an asterisk. We split on that and drop
                         // the first element since it's just group category name
                         // groups.put(it, it.replace("\\s".toRegex(), "").split("*").drop(1))
-                        strGroupData = it.replace("\\s".toRegex(), "")
+                        //strGroupData = it.replace("\\s".toRegex(), "")
+                        strGroupData = it.replace("\\s".toRegex(), " ").drop(1)
                         strGroupData.split("*").drop(1).forEach {
-                            lsGroupData.add(it)
+                            lsGroupData.add(it.trim())
                         }
                     }
                 }
@@ -83,11 +84,35 @@ class windows {
         return groups
     }
 
-    fun getFinishedData(){
-        var output = console("net user")
-        var usersd = userData(output)
-        var groupd = groupData(usersd)
+    fun jsonify(groups_data: MutableMap<String, MutableList<String>> ): String {
 
+        var jsonData = "{ \"userdata\": {"
+
+        groups_data.forEach {
+            var groupData = "\"${it.key}\": ["
+
+            it.value.forEach {
+                groupData += "\"${it}\","
+            }
+
+            groupData = groupData.dropLast(1)
+            groupData += "],"
+            jsonData += groupData
+        }
+
+        jsonData = jsonData.dropLast(1)
+        jsonData += "}}"
+
+        println(jsonData)
+        return "FOO"
+    }
+
+    fun getFinishedData(){
+        val output = console("net user")
+        val usersd = userData(output)
+        val groupd = groupData(usersd)
+
+        println(jsonify(groupd))
     }
 
 
